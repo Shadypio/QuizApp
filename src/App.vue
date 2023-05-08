ee62cf38cc7e1276698df9bc33fab5fa67c4e8cf
Shadypio <enzuccioamendola@gmail.com>
2023-05-08 16:37:55 +0200

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
+    <Nav />
     <RouterView />
 </template>
 
 <script setup>
 import {RouterView} from "vue-router";
+import Nav from "./components/Nav.vue";
 </script>
 
 <style scoped>
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <RouterLink active-class="active" to="/">Home</RouterLink>
+        <RouterLink active-class="active" to="/about">About</RouterLink>
+    </div>
+</template>
+
+<script setup>
+    import { RouterLink } from "vue-router";
+
+</script>
+
+<style scoped>
+    .active {
+        font-weight: 900;
+        color: rgb(153, 104, 14);
+    }
+</style>
